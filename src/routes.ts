c833d8d1b0a02df7478937f3a809448de56a2447
Renato <renato.melo@ab-inbev.com>
2023-02-25 18:41:24 -0300

    feat: create new featured route

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,22 @@
-import { Router } from "express";
+import { Response, Router } from "express";
 import { supabase } from "./database";
+import { ProductRepository } from "./features/shared/product/repositories/ProductRepository";
 
 const router = Router()
+
+router.get('/featured', async (_, response: Response) => {
+  const productRepository = new ProductRepository()
+
+  try {
+    const products = await productRepository.getFeatured()
+
+    return response.status(200).json(products)
+  } catch (error) {
+    console.log(error)
+
+    return response.status(500).send('Something wrong.')
+  }
+})
 
 // create a repo that receives a database (unknown) as argument (dependency injection)
 // so that I can mock the db using vi.fn() to avoid changes on the real db
