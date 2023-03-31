6b5d8e232936f506bb0d9e0772286dadd18edb16
Renato <renato.melo@ab-inbev.com>
2023-03-31 07:22:15 -0300

    chore: config node env variables

--- src/server.ts
+++ src/server.ts
@@ -4,17 +4,23 @@ import Express, { Response } from "express";
 import { mongoConnection } from "./database";
 import router from "./routes";
 
-dotenv.config()
+let corsOrigin = process.env.CLIENT_URL
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+  corsOrigin = "*"
+}
+
 mongoConnection()
 
 const server = Express()
 
 server.use(Express.json())
 server.use(cors({
-  origin: "*"
+  origin: corsOrigin
 }))
 
-server.use('/api', router)
+server.use('/api/v1', router)
 
 server.use((_, response: Response) => {
   return response.status(404).send('There is nothing here. Try "/products"')
