2e4361885be2936b139b33ba231d2a8f9ec9dd2c
Renato <renato.melo@ab-inbev.com>
2023-04-12 21:46:28 -0300

    chore: create message constant for quantity adjusted

---
+++ src/modules/cart/constants/index.ts
@@ -0,0 +1,4 @@
+export const QUANTITY_ADJUSTED_MESSAGE = {
+  title: 'Quantity Adjusted',
+  text: 'Your requested quantity has been adjusted to XXX for the product YYY due to limited stock availability.'
+}
