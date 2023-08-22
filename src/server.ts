0d1b22560e9f1eaa36e87fdc484bff100749b76d
Renato <renato.melo@ab-inbev.com>
2023-08-22 18:09:33 -0300

    chore: enable custom config for cors

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,14 @@ interface AppProps {
   router: Router
 }
 
+function checkOrigin(origin?: string, cb?: Function) {
+  if (!origin || !cb) return
+  if (origin?.includes(process.env.CLIENT_URL ?? '')) cb(null, { origin: true } as any)
+  else cb(Error('CORS error: origin not allowed.', { origin: false } as any))
+}
+
 function app({ connections, router }: AppProps) {
-  let corsOrigin = process.env.CLIENT_URL
+  let corsOrigin: cors.CorsOptions['origin'] = checkOrigin
 
   if (process.env.NODE_ENV !== 'production') {
     dotenv.config()
